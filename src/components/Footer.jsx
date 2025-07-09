c6cb6275fcad3c2734dd442715486a8d1493d7fc
a-bc-dev <biancadragan11@gmail.com>
2025-07-09 21:10:32 +0200

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import '../styles/components/Footer.scss'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <p>&copy; 2025 Re Map. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
