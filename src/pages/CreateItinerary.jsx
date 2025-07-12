ca3d25122b13b77e5fe3edf2a06ec645259c7cf1
a-bc-dev <biancadragan11@gmail.com>
2025-07-12 20:39:52 +0200

    Layout and styles for CreateItinerary page

--- src/pages/CreateItinerary.jsx
+++ src/pages/CreateItinerary.jsx
@@ -1,6 +1,25 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../styles/pages/CreateItinerary.scss';
+
 function CreateItinerary() {
-    return <h1>Create Itinerary Page</h1>
-  }
-  
-  export default CreateItinerary
+  return (
+    <div className="create-itinerary">
+      <Header />
+      <div className="create-itinerary__wrapper">
+        <aside className="create-itinerary__panel">
+          <h2>Search Address</h2>
+          <p>Here will go AddressSearch and MarkerForm.</p>
+        </aside>
+        <main className="create-itinerary__map">
+          <p>Map placeholder</p>
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default CreateItinerary;
+
   
